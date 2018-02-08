b600c894955e5d635786257ca9d4ff506ed74eb6
ORIFJON NARKULOV <orifjon9@gmail.com>
2018-02-08 00:02:22 -0600

    the first animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,25 @@ import { Component, trigger, state, style } from '@angular/core';
   templateUrl: './app.component.html',
   animations: [
     trigger('divState', [
-      state('notmal', style({
+      state('normal', style({
         'background-color': 'red',
         transform: 'translateX(0)'
       })),
       state('highlighted', style({
-        backgroundColor: 'blue',
+        'background-color': 'blue',
         transform: 'translateX(100px)'
       }))
     ])
   ]
 })
 export class AppComponent {
-  state = 'normal';
+  state = 'highlighted';
 
   list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+  }
 
   onAdd(item) {
     this.list.push(item);
